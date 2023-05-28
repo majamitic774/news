0940795e3aa4a9c56d03081fc40863a0a0fb9ef0
maja <majamitic774@gmail.com>
2023-05-28 16:25:46 +0200

    Add log in form

---
+++ public/views/usersLoginForm.php
@@ -0,0 +1,22 @@
+<body>
+    <div class="container-short">
+        <form action="" method="POST" enctype="multipart/form-data">
+            <?php if (isset($_SESSION['token'])) : ?>
+                <input type="hidden" name="token" value="<?php echo $_SESSION['token'] ?>">
+            <?php endif; ?>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" name="email" id="email">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" name="password" id="password">
+            </div>
+            <button type="submit" name="login-user" class="btn btn-primary">Login</button>
+        </form>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
